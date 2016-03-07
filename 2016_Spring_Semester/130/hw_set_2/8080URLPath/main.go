5a653d48b6a3939dd90e26405099905340d908e3
Gavin Haugen <gavinhaugen@mail.fresnostate.edu>
2016-03-07 10:21:03 -0800

    hw 1-3 for hw set 2 done

---
+++ 2016_Spring_Semester/130/hw_set_2/8080URLPath/main.go
@@ -0,0 +1,17 @@
+package main 
+
+import {
+	"net/http"
+	"fmt"
+	"strings"
+}
+
+func PathN(res http.ResponseWriter,req *http.Request){
+	name := strings.Split(req.URL.Path,"/")
+	fmt.Fprint(res, name[len(name)-1])
+}
+
+func main(){
+	http.HandleFunc("/", PathN)
+	http.listenAndServe(":8080", nil)
+}
---
+++ 2016_Spring_Semester/130/hw_set_2/name_entry/main.go
@@ -0,0 +1,29 @@
+package main 
+
+import{
+	"net/http"
+	"fmt"
+}
+
+func PageMaker(res http.ResponseWriter, req *http.Request){
+	fmt.Fprint(res, <!DOCTYPE html>
+					<html>
+					<head>
+						<title></title>
+					</head>
+
+					<body>
+						<form>
+							Input name: <br>
+							<input type="text" name="name"><br>
+						</form>
+					</body>
+					</html>)
+
+	fmt.Fprint(res,req.FormValue("name"))
+}
+
+func main(){
+	http.HandleFunc("/", PageMaker)
+	http.ListenAndServe(":8080", nil)
+}
