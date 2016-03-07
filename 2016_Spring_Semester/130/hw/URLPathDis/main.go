9576827b9cafdb8add83b70ee2945a2ac0f54213
Gavin Haugen <gavinhaugen@mail.fresnostate.edu>
2016-03-06 19:54:35 -0800

    URL path excersize done

---
+++ 2016_Spring_Semester/130/hw/URLPathDis/main.go
@@ -0,0 +1,17 @@
+// displays the URL path in a web page
+
+package main
+
+import{
+	"net/http"
+	"io"
+}
+
+func URLPath(res http.ResponseWriter,req *http.Request){
+	io.WriteString(res, req.URL.Path)
+}
+
+func main(){
+	http.HandleFunc("/",URLPath)
+	http.listenAndServe(":8080",nil)
+}
